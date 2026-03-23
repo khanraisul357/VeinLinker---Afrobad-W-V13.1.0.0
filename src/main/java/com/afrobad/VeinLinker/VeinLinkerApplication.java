efa696fecd9b29abf5edffe3e5ee5ce91dace446
Md Raisul Islam Khan <khanraisul357@gmail.com>
2026-03-23 19:45:09 +0600

    Project Files Added

---
+++ src/main/java/com/afrobad/VeinLinker/VeinLinkerApplication.java
@@ -0,0 +1,13 @@
+package com.afrobad.VeinLinker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeinLinkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeinLinkerApplication.class, args);
+	}
+
+}
