b60c01c249f59d900140a3aabc2b92e0a6a8b2eb
Md Raisul Islam Khan <khanraisul357@gmail.com>
2026-03-29 14:13:48 +0600

    query added

--- src/main/resources/db/migration/V2_create_role_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE role (
-    roleID INT PRIMARY KEY AUTO_INCREMENT,
-    internal_userID BIGINT NOT NULL,
-    role_name ENUM('Donor', 'Receiver') NOT NULL,
-    -- Foreign Key Constraint
-	FOREIGN KEY (internal_userID) REFERENCES Users(internal_userID)
-	ON DELETE CASCADE
-	ON UPDATE CASCADE
-);
