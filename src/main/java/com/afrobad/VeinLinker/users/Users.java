bc2beeb33b5c272a7404a72ca64a4f2f4a31fdbf
Md Raisul Islam Khan <khanraisul357@gmail.com>
2026-04-02 17:43:56 +0600

    Users.java(entity class) updated

--- src/main/java/com/afrobad/VeinLinker/users/Users.java
+++ src/main/java/com/afrobad/VeinLinker/users/Users.java
@@ -1,5 +1,30 @@
 package com.afrobad.VeinLinker.users;
 
-public class Users {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import org.hibernate.annotations.CreationTimestamp;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="users")
+public class Users{
+	
+	// -------------------------------------------------------------------------
+    // PRIMARY KEY — internal_userID
+    // BIGINT AUTO_INCREMENT in MySQL → Long + @GeneratedValue in Java.
+    // This column is NEVER exposed to the outside world.
+    // It is used only for internal DB joins and foreign keys in other tables.
+    // ------
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name = "internal_userID")
+	private long internal_userID;
+	
 }
+    
