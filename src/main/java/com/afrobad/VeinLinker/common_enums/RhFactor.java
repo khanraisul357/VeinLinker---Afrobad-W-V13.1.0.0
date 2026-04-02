88554c3e48c1565ed6d8db557f51439b93a1c6ca
Md Raisul Islam Khan <khanraisul357@gmail.com>
2026-04-02 18:48:15 +0600

    enum RhFactor.java added

---
+++ src/main/java/com/afrobad/VeinLinker/common_enums/RhFactor.java
@@ -0,0 +1,10 @@
+package com.afrobad.VeinLinker.common_enums;
+
+public enum RhFactor {
+    POSITIVE, NEGATIVE;
+
+    // MySQL stores '+' or '-' but Java enum names can't be symbols.
+    // The @Column columnDefinition handles the DB side.
+    // In your Service, map "+" → RhFactor.POSITIVE before saving.
+	//OR, change enum in MySQL "POSITIVE" & "NEGATIVE"
+}
