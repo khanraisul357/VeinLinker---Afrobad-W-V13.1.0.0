908d72076a25983c9214872bea145963bf13c616
Md Raisul Islam Khan <khanraisul357@gmail.com>
2026-05-30 00:22:38 +0600

    enum created

---
+++ src/main/java/com/afrobad/VeinLinker/registrationandlogin/userverification/enums/VerificationStage.java
@@ -0,0 +1,9 @@
+package com.afrobad.VeinLinker.registrationandlogin.userverification.enums;
+
+
+public enum VerificationStage {
+    EMAIL_PENDING,
+    PHONENUMBER_PENDING,
+    DOCUMENT_PENDING,
+    COMPLETED
+}
