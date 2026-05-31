b9ecdd053bd31f2d8cd4ad40611b3e60735d56f3
Md Raisul Islam Khan <khanraisul357@gmail.com>
2026-05-31 23:59:23 +0600

    updated

---
+++ src/main/java/com/afrobad/VeinLinker/registrationandlogin/uploadedfile/enums/FileType.java
@@ -0,0 +1,9 @@
+package com.afrobad.VeinLinker.registrationandlogin.uploadedfile.enums;
+
+public enum FileType {
+    PROFILE_IMAGE,
+    NID_FRONT,
+    NID_BACK,
+    PASSPORT_FRONT,
+    PASSPORT_BACK
+}
