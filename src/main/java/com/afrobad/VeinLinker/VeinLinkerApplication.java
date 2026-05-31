fc160f0a26df237cd1a66f8028032e59a67e6b1e
Md Raisul Islam Khan <khanraisul357@gmail.com>
2026-06-01 02:18:41 +0600

    annotation to enable cache added

--- src/main/java/com/afrobad/VeinLinker/VeinLinkerApplication.java
+++ src/main/java/com/afrobad/VeinLinker/VeinLinkerApplication.java
@@ -2,10 +2,12 @@ package com.afrobad.VeinLinker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableCaching
 public class VeinLinkerApplication {
 
 	public static void main(String[] args) {
