a7a128e3b0fbfc353b5aefdbe06940d00b22c2f4
Md Raisul Islam Khan <khanraisul357@gmail.com>
2026-06-01 23:33:35 +0600

    created

--- src/main/java/com/afrobad/VeinLinker/registrationandlogin/users/controller/UsersController.java
+++ src/main/java/com/afrobad/VeinLinker/registrationandlogin/users/controller/UsersController.java
@@ -1,5 +1,28 @@
-package com.afrobad.VeinLinker.users;
+// For view profile, edit profile
 
+package com.afrobad.VeinLinker.registrationandlogin.users.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.afrobad.VeinLinker.registrationandlogin.users.dto.RegistrationForm1Request;
+import com.afrobad.VeinLinker.registrationandlogin.users.dto.RegistrationForm1Response;
+
+import jakarta.validation.Valid;
+
+
+@RestController
 public class UsersController {
+	
+	
+	
+//	@PostMapping("/register/form-1")
+//	public String processStep1(@Valid @RequestBody RegistrationForm1Request request) {
+//	    // If validation fails, Spring automatically throws MethodArgumentNotValidException 
+//	    // and returns a 400 Bad Request.
+//	    
+//	    // Process temporary saving logic here
+//	    return "Step 1 complete. Please fill you health informations";
+//	}
 
 }
