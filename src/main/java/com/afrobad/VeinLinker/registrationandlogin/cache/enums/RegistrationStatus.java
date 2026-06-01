842c1ebe74162e5d59671cd38e3854c78ee4c399
Md Raisul Islam Khan <khanraisul357@gmail.com>
2026-06-01 23:30:40 +0600

    moved

---
+++ src/main/java/com/afrobad/VeinLinker/registrationandlogin/cache/enums/RegistrationStatus.java
@@ -0,0 +1,18 @@
+package com.afrobad.VeinLinker.registrationandlogin.cache.enums;
+
+public enum RegistrationStatus {
+
+    IN_PROGRESS,
+
+    FORM1_COMPLETED,
+
+    FORM2_COMPLETED,
+
+    FORM3_COMPLETED,
+
+    COMPLETED,
+
+    ABANDONED,
+
+    EXPIRED
+}
