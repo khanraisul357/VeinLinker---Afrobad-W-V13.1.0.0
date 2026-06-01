0f891f49d05c9e8c2dc00afd81af148065542405
Md Raisul Islam Khan <khanraisul357@gmail.com>
2026-06-01 23:33:50 +0600

    created

---
+++ src/main/java/com/afrobad/VeinLinker/registrationandlogin/cache/UploadedDocumentDraft.java
@@ -0,0 +1,27 @@
+package com.afrobad.VeinLinker.registrationandlogin.cache;
+
+import com.afrobad.VeinLinker.registrationandlogin.uploadedfile.enums.*;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UploadedDocumentDraft {
+
+    private String fileName;
+
+    private String originalFileName;
+
+    private String fileUrl;
+
+    private Format fileFormat;
+
+    private Long size;
+
+    private FileType documentType;
+
+    private UploadStatus uploadStatus;
+}
