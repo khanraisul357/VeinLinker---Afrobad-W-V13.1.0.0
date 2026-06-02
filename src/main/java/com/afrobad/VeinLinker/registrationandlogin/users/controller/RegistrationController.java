a51c3f8f4d2c347b9ab0f6027aa40e189e3c012b
Md Raisul Islam Khan <khanraisul357@gmail.com>
2026-06-02 23:42:08 +0600

    creataed

---
+++ src/main/java/com/afrobad/VeinLinker/registrationandlogin/users/controller/RegistrationController.java
@@ -0,0 +1,35 @@
+// Handles Form 1, 2, 3 endpoints
+
+package com.afrobad.VeinLinker.registrationandlogin.users.controller;
+
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+
+import org.springframework.web.bind.annotation.*;
+
+import com.afrobad.VeinLinker.registrationandlogin.users.dto.*;
+// handles business logic (multi-step registration flow)
+
+import com.afrobad.VeinLinker.registrationandlogin.users.service.RegistrationService;
+
+import jakarta.validation.*;
+
+@RestController
+public class RegistrationController {
+	
+	@Autowired
+	private RegistrationService multiStepServices;
+	
+	@PostMapping("register/form-1")
+	public ResponseEntity<RegistrationForm1Response> form1Request(@Valid @RequestBody RegistrationForm1Request request){
+		
+		multiStepServices.form1(request);
+		
+		RegistrationForm1Response response=new RegistrationForm1Response();
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);// 201 created(new source created)
+	}
+
+}
