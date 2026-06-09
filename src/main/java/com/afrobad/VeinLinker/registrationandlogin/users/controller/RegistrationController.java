4b2652482342012a71476984d3c7bdf1373fc018
Md Raisul Islam Khan <khanraisul357@gmail.com>
2026-06-09 20:35:38 +0600

    form-2 api endpoint added

--- src/main/java/com/afrobad/VeinLinker/registrationandlogin/users/controller/RegistrationController.java
+++ src/main/java/com/afrobad/VeinLinker/registrationandlogin/users/controller/RegistrationController.java
@@ -9,6 +9,7 @@ import org.springframework.http.*;
 
 import org.springframework.web.bind.annotation.*;
 
+import com.afrobad.VeinLinker.registrationandlogin.cache.drafts.RegistrationDraft;
 import com.afrobad.VeinLinker.registrationandlogin.users.dto.*;
 // handles business logic (multi-step registration flow)
 
@@ -22,14 +23,26 @@ public class RegistrationController {
 	@Autowired
 	private RegistrationService multiStepServices;
 	
+//	@Autowired 
+//	private RegistrationDraft draft;
+	
 	@PostMapping("/register/form-1")
 	public ResponseEntity<RegistrationForm1Response> form1Request(@Valid @RequestBody RegistrationForm1Request request){
 			
 		
-		RegistrationForm1Response response=multiStepServices.processForm1(request);;
+		RegistrationForm1Response response=multiStepServices.processForm1(request);
 		
 		return ResponseEntity.status(HttpStatus.CREATED).body(response);// 201 created(new source created)
 	}
+	
+//	@PostMapping("/register/form-2")
+//	public ResponseEntity<RegistrationForm2Response> form2Request(@Valid @RequestBody RegistrationForm2Request request){
+//		
+//		
+//		RegistrationForm2Response response=multiStepServices.processForm2(draft.getEmail(),request);
+//		return ResponseEntity.status(HttpStatus.CREATED).body(response);// 201 created(new source created)
+//
+//	}
 	
 	
 
