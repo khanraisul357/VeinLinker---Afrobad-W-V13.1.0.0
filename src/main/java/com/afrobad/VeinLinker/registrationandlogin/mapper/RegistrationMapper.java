bdece62432a46f6e90f3d0634914e7963bb88322
Md Raisul Islam Khan <khanraisul357@gmail.com>
2026-06-16 19:36:55 +0600

    error fixed

--- src/main/java/com/afrobad/VeinLinker/registrationandlogin/mapper/RegistrationMapper.java
+++ src/main/java/com/afrobad/VeinLinker/registrationandlogin/mapper/RegistrationMapper.java
@@ -1,16 +1,21 @@
 package com.afrobad.VeinLinker.registrationandlogin.mapper;
 
 
+import java.util.UUID;
+
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.ReportingPolicy;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.afrobad.VeinLinker.registrationandlogin.cache.drafts.RegistrationDraft;
+import com.afrobad.VeinLinker.registrationandlogin.cache.drafts.UploadedFileDraft;
+import com.afrobad.VeinLinker.registrationandlogin.uploadedfile.enums.*;
 import com.afrobad.VeinLinker.registrationandlogin.users.dto.*;
 
 
-
+import lombok.*;
 
 /*componentModel = "spring" allows to create object of this mapper and store it to IOC Container & inject it.
 Simply I am telling spring to manage lifecycle of object of this mapper.
@@ -21,14 +26,34 @@ Simply I am telling spring to manage lifecycle of object of this mapper.
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface RegistrationMapper {
 
-	@Mapping(target = "documents", ignore = true)
+	@Mapping(target = "uploadedDocuments", ignore = true)
     RegistrationDraft form1RequestDtoToDraft(RegistrationForm1Request request);
     
     //Form 2Updates the EXISTNG draft loaded from Redis. It does NOT touch Form 1 data.
     void updateDraftWithForm2(RegistrationForm2Request request, @MappingTarget RegistrationDraft draft);
 
-    // Form 3: Updates the EXISTING draft loaded from Redis with Form 3 data before final DB push.
-    void updateDraftWithForm3(RegistrationForm3Request request, @MappingTarget RegistrationDraft draft);
+      // Form 3: Updates the EXISTING draft loaded from Redis with Form 3 data before final DB push.
+    default UploadedFileDraft MultipartFileToUploadedDocumentDraft(MultipartFile file, FileType fileType, Format format) {
+    	
+    	return UploadedFileDraft.builder()
+
+                .fileName(UUID.randomUUID().toString())
+
+                .originalFileName(file.getOriginalFilename())
+
+                .size(file.getSize())
+
+                .fileFormat(format)
+
+                .documentType(fileType)
+
+                .uploadStatus(UploadStatus.PENDING)
+
+                .fileUrl(null)
+
+                .build();
+    	
+    }
 
     
 }
