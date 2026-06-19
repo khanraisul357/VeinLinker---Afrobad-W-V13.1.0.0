29836f08b81393b7119d39274d317d0e2483201d
Md Raisul Islam Khan <khanraisul357@gmail.com>
2026-06-19 23:58:45 +0600

    updated

--- src/main/java/com/afrobad/VeinLinker/registrationandlogin/users/repository/UsersRepository.java
+++ src/main/java/com/afrobad/VeinLinker/registrationandlogin/users/repository/UsersRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.afrobad.VeinLinker.registrationandlogin.users.Users;
+import com.afrobad.VeinLinker.registrationandlogin.users.entity.Users;
 
 import java.util.Optional;
 
