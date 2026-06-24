97c3ee0b25347f8ddf3b9a5a92ecf532023bca9a
Md Raisul Islam Khan <khanraisul357@gmail.com>
2026-06-24 14:34:23 +0600

    submitRegistrationForm() method created

--- src/main/java/com/afrobad/VeinLinker/registrationandlogin/users/service/RegistrationService.java
+++ src/main/java/com/afrobad/VeinLinker/registrationandlogin/users/service/RegistrationService.java
@@ -5,9 +5,9 @@ package com.afrobad.VeinLinker.registrationandlogin.users.service;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.UUID;
+import java.util.stream.IntStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -17,11 +17,25 @@ import com.afrobad.VeinLinker.registrationandlogin.cache.drafts.UploadedFileDraf
 import com.afrobad.VeinLinker.registrationandlogin.cache.enums.RegistrationStatus;
 import com.afrobad.VeinLinker.registrationandlogin.cache.service.RegistrationCacheService;
 import com.afrobad.VeinLinker.registrationandlogin.mapper.RegistrationMapper;
+import com.afrobad.VeinLinker.registrationandlogin.uploadedfile.entity.Files;
+import com.afrobad.VeinLinker.registrationandlogin.uploadedfile.entity.UserFile;
 import com.afrobad.VeinLinker.registrationandlogin.uploadedfile.enums.FileType;
 import com.afrobad.VeinLinker.registrationandlogin.uploadedfile.enums.Format;
+import com.afrobad.VeinLinker.registrationandlogin.uploadedfile.enums.UploadStatus;
+import com.afrobad.VeinLinker.registrationandlogin.uploadedfile.repository.FileRepository;
+import com.afrobad.VeinLinker.registrationandlogin.uploadedfile.repository.UserFileRepository;
 import com.afrobad.VeinLinker.registrationandlogin.users.dto.*;
+import com.afrobad.VeinLinker.registrationandlogin.users.entity.Users;
+import com.afrobad.VeinLinker.registrationandlogin.users.enums.AccountStatus;
 import com.afrobad.VeinLinker.registrationandlogin.users.enums.Role;
+import com.afrobad.VeinLinker.registrationandlogin.users.enums.UserStatus;
 import com.afrobad.VeinLinker.registrationandlogin.users.repository.UsersRepository;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.Validator;
+import jakarta.validation.ConstraintViolation;
+import java.util.Set;
+
 import java.util.List;
 
 import org.springframework.web.multipart.MultipartFile;
@@ -31,6 +45,11 @@ public class RegistrationService {
 	@Autowired
 	private UsersRepository repository;
 	
+	@Autowired
+	private FileRepository fileRepository;
+	
+	@Autowired
+	private UserFileRepository userFileRepository;
 	
 	@Autowired
     private BCryptPasswordEncoder passwordEncoder;
@@ -40,6 +59,9 @@ public class RegistrationService {
 	
 	@Autowired
     private RegistrationCacheService cacheService;
+	
+	@Autowired
+	private Validator validator;
 	
 	// Isolated Business Validation Method
     private void validateForm1(RegistrationForm1Request request) {
@@ -57,23 +79,7 @@ public class RegistrationService {
         
     }
     
-    //Method to get format(jpg,jpeg,png,pdf) of the uploaded file
-    private Format getFormat(MultipartFile file) {
-
-        String originalName = file.getOriginalFilename();
-
-        if (originalName == null) {
-            throw new IllegalArgumentException("File name is missing.");
-        }
-
-        String extension =
-                originalName.substring(
-                        originalName.lastIndexOf('.') + 1
-                ).toUpperCase();
-
-        return Format.valueOf(extension);
-    }
-	
+    	
 	public RegistrationForm1Response processForm1(RegistrationForm1Request request) {
 		
 		//Run the business validations first(whether users email or phone number exists in DB)
@@ -101,10 +107,7 @@ public class RegistrationService {
         draft.setUpdatedAt(now);
         
         
-
-        /*Calling method RegistrationCacheService.saveDraft() to save form 1 data temporarily to Redis 
-        using email as the unique key
-        */
+        //Calling method RegistrationCacheService.saveDraft() to save form 1 data temporarily to Redis using email as the unique key
         cacheService.saveDraft(request.getEmail(), draft);
 		
         
@@ -129,8 +132,10 @@ public class RegistrationService {
 	        throw new IllegalStateException("Registration session expired or not found.");
 	    }
 
-	    //MapStruct updates ONLY the fields present in Form 2 Request.
-	    //Your Form 1 data (email, phone, passwordHash) remains completely untouched and safe!
+	    /*
+	     * MapStruct updates ONLY the fields present in Form 2 Request.
+	     * Form 1 data (email, phone, passwordHash) remains completely untouched and safe!
+	    */
 	    mapper.updateDraftWithForm2(request, existingDraft);
 
 	    //Update the step counter metadata manually
@@ -150,6 +155,23 @@ public class RegistrationService {
 	}
 	
 	
+	//Method to get format(jpg,jpeg,png,pdf) of the uploaded file
+    private Format getFormat(MultipartFile file) {
+
+        String originalName = file.getOriginalFilename();
+
+        if (originalName == null) {
+            throw new IllegalArgumentException("File name is missing.");
+        }
+
+        String extension =
+                originalName.substring(
+                        originalName.lastIndexOf('.') + 1
+                ).toLowerCase();
+
+        return Format.valueOf(extension);
+    }
+
 	
 	public RegistrationDraft submitForm3(RegistrationForm3Request request, MultipartFile frontImage, MultipartFile backImage, MultipartFile profileImage ) {
 		
@@ -159,8 +181,10 @@ public class RegistrationService {
 	        throw new IllegalStateException("Registration session expired or not found.");
 	    }
 	    
-	    //Mapping each file request(MultipartFIle) to UploadedFileDraft
-	    //Why?: I have to store each uploaded file as UploadedFileDraft object in a list, since list type is <UploadedFileDraft>
+	    /*
+	     * Mapping each file request(MultipartFIle) to UploadedFileDraft
+	     * Why?: I have to store each uploaded file as UploadedFileDraft object in a list, since list type is <UploadedFileDraft>
+	    */
 	    UploadedFileDraft frontDraft= mapper.MultipartFileToUploadedDocumentDraft(frontImage,FileType.NID_FRONT,getFormat(frontImage));
 	    UploadedFileDraft backDraft= mapper.MultipartFileToUploadedDocumentDraft(backImage,FileType.NID_BACK,getFormat(backImage));
 	    UploadedFileDraft profileDraft= mapper.MultipartFileToUploadedDocumentDraft(profileImage,FileType.PROFILE_IMAGE,getFormat(profileImage));
@@ -181,15 +205,123 @@ public class RegistrationService {
 	    
 	}
 	
-//	public boolean submitRegistration(String email) {
-//		//Fetch the complete draft from Redis through email as key.
-//	    RegistrationDraft existingDraft = cacheService.getDraft(email);
-//	    if (existingDraft == null) {
-//	        throw new IllegalStateException("Registration session expired or not found.");
-//	    }
-//	    
-//	    return true;
-//	}
+	//Method to validate draft fetched from redis before storing it to DB permanently
+	private void validateRegistrationDraft(RegistrationDraft draft) {
+		
+        /*
+         * This single line triggers EVERY annotation rule inside your draft object
+         * Validator.validate() method handing draft to hibernet validator & triggers hibernet validator.
+         * Scans RegistrationDraft Fields & looks for JVA.
+         * Returns Set containing ConstraintViolation.
+         * If no field validation failed, returns set containing zero/size=0/empty ConstraintViolation object(violations=0 object)
+         * If there 5 field validation failed, returns set containing five/size=5/not empty ConstraintViolation object(violations=5 objects) 
+		 */
+        Set<ConstraintViolation<RegistrationDraft>> violations = validator.validate(draft);
+        
+        // If there are any errors, deal with them immediately
+        if (!violations.isEmpty()) {
+            // Option A: Extract the first error message and throw an exception
+            String firstErrorMessage = violations.iterator().next().getMessage();
+            throw new IllegalArgumentException(firstErrorMessage);
+        }
+	}
+	
+	//Method to generate publicUserId
+	private String generatePublicId(long internalId) {
+	    return String.format("VL-%07d", internalId);
+	}
+	
+	@Transactional
+	public Users submitRegistrationForm(String email) {
+		
+		//Step-1:Fetch the complete draft from Redis through email as key.
+	    RegistrationDraft draft = cacheService.getDraft(email);
+	    if (draft == null) {
+	        throw new IllegalStateException("Registration session expired or not found.");
+	    }
+	    
+	    //Step-2:Validate fetched draft
+	    validateRegistrationDraft(draft);
+	    
+	    //Step-3: Creating Users Object-->Map Users to RegistrationDraft(Converting RegistrationDraft into Users)
+	    Users user=mapper.draftToUsers(draft);
+	    
+	    // =========================================================================
+	    // 1.Identity Fields Initialization
+	    // =========================================================================
+	    
+	    //MySQL generates internalUserId after insert operation(Users object saved to DB)
+	    //Generates encryptedUserId
+	    user.setEncryptedUserId(UUID.randomUUID().toString());
+	    //publicUserID generated only after internalUserId generated by MySQL.
+
+	    // ======================================================
+	    // 2. Lifecycle state Fields (initial state)
+	    // ======================================================
+
+	    user.setIsVerified(UserStatus.PENDING);
+	    user.setAccountStatus(AccountStatus.INACTIVE);
+	    user.setLastActiveMode(null); // no activity yet
+
+	    // ======================================================
+	    // 3. Stat Fields (safety net, even if @Builder.Default exists)
+	    // ======================================================
+
+	    user.setDonationCount(0);
+	    user.setReceivingCount(0);
+	    user.setPoints(0);
+	    user.setLevel(0);
+	    user.setReliabilityScore(100);
+
+	    // ======================================================
+	    // 4. Save first (important for public ID generation)
+	    // ======================================================
+
+//	    user = repository.save(user);
+
+	    // ======================================================
+	    // 5. Public ID generation (depends on DB-generated ID)
+	    // ======================================================
+	    user.setPublicUserId(generatePublicId(user.getInternalUserId()));
+	    
+	    user = repository.save(user);
+
+	    
+	    
+	    //Step-4:Converting List of UploadedFileDraft to List of Files & Creating Files Object
+	    
+	    List<Files> files =draft.getUploadedDocuments().stream().map(mapper::draftToFiles).toList();
+	  
+	    //Step-5: Saving list of Files to DB
+        fileRepository.saveAll(files);
+        
+        //Step-6:Creating List of UserFile Object
+        List<UserFile> userFiles = new ArrayList<>();
+        
+        for(int i=0;i<files.size();i++) {
+
+          UploadedFileDraft fileDraft =draft.getUploadedDocuments().get(i);//extracting object from list(uploadedDocuments)
+
+          Files file =files.get(i);//extracting object from list(files)
+
+          UserFile userFile=UserFile.builder()//userFile is single UserFile Object
+        	         .user(user)
+        	         .file(file)
+        	         .documentType(fileDraft.getDocumentType())
+        	         .status(UploadStatus.PENDING)
+        	         .build();
+        	            
+          userFiles.add(userFile);
+        }
+        
+        //Step-8: Saving list of UserFile to DB
+        userFileRepository.saveAll(userFiles);
+        
+        //Delete draft from redis
+        
+        return user;
+	    
+	}
 	
 	
 
