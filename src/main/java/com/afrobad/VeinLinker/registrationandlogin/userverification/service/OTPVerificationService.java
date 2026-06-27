ad96b61409fb792fd90c111ee87604d1eb50324a
Md Raisul Islam Khan <khanraisul357@gmail.com>
2026-06-28 00:56:47 +0600

    created

---
+++ src/main/java/com/afrobad/VeinLinker/registrationandlogin/userverification/service/OTPVerificationService.java
@@ -0,0 +1,8 @@
+package com.afrobad.VeinLinker.registrationandlogin.userverification.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OTPVerificationService {
+
+}
