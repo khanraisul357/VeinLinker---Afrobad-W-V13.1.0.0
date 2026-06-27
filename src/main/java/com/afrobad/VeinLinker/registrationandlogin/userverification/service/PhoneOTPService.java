35180087bb0032afc7df263c20c5ddafb4405c39
Md Raisul Islam Khan <khanraisul357@gmail.com>
2026-06-28 04:48:27 +0600

    created to send otp phone sms

---
+++ src/main/java/com/afrobad/VeinLinker/registrationandlogin/userverification/service/PhoneOTPService.java
@@ -0,0 +1,15 @@
+//This class responsible for sending OTP to SMS(Phone Number) of users
+
+package com.afrobad.VeinLinker.registrationandlogin.userverification.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PhoneOTPService {
+	
+	public String sendOTP(String Email,String emailOTP){
+		return emailOTP;
+		
+	}
+
+}
