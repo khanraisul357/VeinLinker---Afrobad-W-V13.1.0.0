3abcab6655ef265319f5fe0a9265e9c3ec1e268c
Md Raisul Islam Khan <khanraisul357@gmail.com>
2026-06-29 17:04:06 +0600

    RequestDTO Created

---
+++ src/main/java/com/afrobad/VeinLinker/registrationandlogin/userverification/dto/EmailVerificationRequestDTO.java
@@ -0,0 +1,13 @@
+package com.afrobad.VeinLinker.registrationandlogin.userverification.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+public class EmailVerificationRequestDTO {
+	
+	private String email;
+	private String otp;
+	
+
+}
