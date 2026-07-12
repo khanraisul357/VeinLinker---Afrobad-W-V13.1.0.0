f678c68420885050d3965ef06e843f5e24a58217
Md Raisul Islam Khan <khanraisul357@gmail.com>
2026-07-12 23:54:40 +0600

    changed parameters of its method startVerification()

--- src/main/java/com/afrobad/VeinLinker/registrationandlogin/cache/service/OTPCacheService.java
+++ src/main/java/com/afrobad/VeinLinker/registrationandlogin/cache/service/OTPCacheService.java
@@ -2,42 +2,45 @@ package com.afrobad.VeinLinker.registrationandlogin.cache.service;
 
 import java.time.Duration;
 
+import org.mapstruct.Qualifier;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OTPCacheService {
 
 	    @Autowired
-	    private RedisTemplate<String, Object> redisTemplate;
+	    private StringRedisTemplate redisTemplateForOTP;
+	    
+	    private static final Duration OTP_EXPIRATION_MINUTES = Duration.ofMinutes(2);;//2 Minutes
 
-	    private static final long OTP_EXPIRATION_MINUTES = 5;//After 5 minutes OTP will expire
 
 	    public void saveEmailOTP(String email, String otp) {
 
-	        redisTemplate.opsForValue().set( "EMAIL_OTP:" + email, otp,Duration.ofMinutes(OTP_EXPIRATION_MINUTES));
+	        redisTemplateForOTP.opsForValue().set( "EMAIL_OTP:" + email, otp,OTP_EXPIRATION_MINUTES);
 	    }
 
 	    public void savePhoneOTP(String phone, String otp) {
 
-	        redisTemplate.opsForValue().set("PHONE_OTP:" + phone,otp,Duration.ofMinutes(OTP_EXPIRATION_MINUTES));
+	        redisTemplateForOTP.opsForValue().set("PHONE_OTP:" + phone,otp,OTP_EXPIRATION_MINUTES);
 	    }
 
 	    public String getEmailOTP(String email) {
-	        return (String) redisTemplate.opsForValue().get("EMAIL_OTP:" + email);
+	        return (String) redisTemplateForOTP.opsForValue().get("EMAIL_OTP:" + email);
 	    }
 
 	    public String getPhoneOTP(String phone) {
-	        return (String) redisTemplate.opsForValue().get("PHONE_OTP:" + phone);
+	        return (String) redisTemplateForOTP.opsForValue().get("PHONE_OTP:" + phone);
 	    }
 
 	    public void deleteEmailOTP(String email) {
-	        redisTemplate.delete("EMAIL_OTP:" + email);
+	        redisTemplateForOTP.delete("EMAIL_OTP:" + email);
 	    }
 
 	    public void deletePhoneOTP(String phone) {
-	        redisTemplate.delete("PHONE_OTP:" + phone);
+	        redisTemplateForOTP.delete("PHONE_OTP:" + phone);
 	    }
 
 }
