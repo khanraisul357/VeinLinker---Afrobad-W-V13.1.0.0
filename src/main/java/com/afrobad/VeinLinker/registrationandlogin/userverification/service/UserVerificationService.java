34581466370604fafae06cf6a68fea37f0767379
Md Raisul Islam Khan <khanraisul357@gmail.com>
2026-07-15 03:43:26 +0600

    called method of UserVerificationService commented

---
+++ src/main/java/com/afrobad/VeinLinker/registrationandlogin/userverification/service/UserVerificationService.java
@@ -0,0 +1,12 @@
+package com.afrobad.VeinLinker.registrationandlogin.userverification.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserVerificationService {
+	
+	public void verifyUser() {
+		
+	}
+
+}
