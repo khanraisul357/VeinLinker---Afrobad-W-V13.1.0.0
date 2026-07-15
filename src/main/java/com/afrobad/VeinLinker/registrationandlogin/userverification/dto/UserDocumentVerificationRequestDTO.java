f2a1f89d59fd07e670b7360de895b9e9d7a858d1
Md Raisul Islam Khan <khanraisul357@gmail.com>
2026-07-16 00:42:16 +0600

    created and necessary fields added, lombok annotations used

--- src/main/java/com/afrobad/VeinLinker/registrationandlogin/userverification/dto/UserDocumentVerificationRequestDTO.java
+++ src/main/java/com/afrobad/VeinLinker/registrationandlogin/userverification/dto/UserDocumentVerificationRequestDTO.java
@@ -2,14 +2,16 @@ package com.afrobad.VeinLinker.registrationandlogin.userverification.dto;
 
 import lombok.*;
 
+import com.afrobad.VeinLinker.registrationandlogin.uploadedfile.enums.VerificationDocumentType;
 import com.afrobad.VeinLinker.registrationandlogin.userverification.enums.VerificationDocumentStatus;
 
 @Getter
 @Setter
 public class UserDocumentVerificationRequestDTO {
 	
+	    private VerificationDocumentType verificationDocumentType;
 
-	    private VerificationDocumentStatus status;
+	    private VerificationDocumentStatus documentVerificationStatus;
 
 	    private String rejectionReason;
 
