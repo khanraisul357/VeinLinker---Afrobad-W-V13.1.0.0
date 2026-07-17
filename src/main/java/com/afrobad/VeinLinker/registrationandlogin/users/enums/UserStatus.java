4c416ea426a77c49e8d6e2d09144c8472395011d
Md Raisul Islam Khan <khanraisul357@gmail.com>
2026-07-17 18:45:46 +0600

    enum created and added

--- src/main/java/com/afrobad/VeinLinker/registrationandlogin/users/enums/UserStatus.java
+++ src/main/java/com/afrobad/VeinLinker/registrationandlogin/users/enums/UserStatus.java
@@ -2,9 +2,6 @@ package com.afrobad.VeinLinker.registrationandlogin.users.enums;
 
 public enum UserStatus {
 
-	    FORM1_COMPLETED,
-	    FORM2_COMPLETED,
-	    FORM3_COMPLETED,
 	    PENDING,
 	    VERIFIED,
 	    REJECTED
