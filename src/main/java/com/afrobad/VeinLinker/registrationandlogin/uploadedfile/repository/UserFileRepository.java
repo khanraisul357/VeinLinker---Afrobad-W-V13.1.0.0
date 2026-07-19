a08e0d83bbcbd06a4069dcc6e092b922f560ebc4
Md Raisul Islam Khan <khanraisul357@gmail.com>
2026-07-19 22:53:11 +0600

    added

--- src/main/java/com/afrobad/VeinLinker/registrationandlogin/uploadedfile/repository/UserFileRepository.java
+++ src/main/java/com/afrobad/VeinLinker/registrationandlogin/uploadedfile/repository/UserFileRepository.java
@@ -1,9 +1,15 @@
 package com.afrobad.VeinLinker.registrationandlogin.uploadedfile.repository;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.afrobad.VeinLinker.registrationandlogin.uploadedfile.entity.UserFile;
+import com.afrobad.VeinLinker.registrationandlogin.users.entity.Users;
+import java.util.List;
+import java.util.Optional;
 
 public interface UserFileRepository extends JpaRepository<UserFile,Long>{
+	
+	Optional<List<UserFile>> findByUser(Users user);
 
 }
