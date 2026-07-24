ecc042509b670b8c5302a195799f7325f2eb11f6
Md Raisul Islam Khan <khanraisul357@gmail.com>
2026-07-24 23:57:02 +0600

    created

--- src/main/java/com/afrobad/VeinLinker/registrationandlogin/userauthentication/controller/AuthenticationController.java
+++ src/main/java/com/afrobad/VeinLinker/registrationandlogin/userauthentication/controller/AuthenticationController.java
@@ -2,6 +2,9 @@
 
 package com.afrobad.VeinLinker.registrationandlogin.userauthentication.controller;
 
+import javax.security.auth.login.AccountNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,13 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.function.EntityResponse;
 
 import com.afrobad.VeinLinker.registrationandlogin.userauthentication.dto.*;
+import com.afrobad.VeinLinker.registrationandlogin.userauthentication.service.AuthenticationService;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthenticationController {
+	
+	@Autowired
+	private AuthenticationService authenticationService;
 
-	@PostMapping("login")
-	private EntityResponse<?> login(@RequestBody LoginRequestDTO request){
+	@PostMapping("/login")
+	private EntityResponse<?> login(@RequestBody LoginRequestDTO request) throws AccountNotFoundException{
+		authenticationService.startLogin(request);
 		return null;
 	}
 }
