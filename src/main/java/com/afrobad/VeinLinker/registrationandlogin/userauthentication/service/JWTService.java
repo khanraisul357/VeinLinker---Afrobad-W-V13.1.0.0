5ef928022900663935166cd2443cd3dd822ee641
Md Raisul Islam Khan <khanraisul357@gmail.com>
2026-08-02 23:56:47 +0600

    created

---
+++ src/main/java/com/afrobad/VeinLinker/registrationandlogin/userauthentication/service/JWTService.java
@@ -0,0 +1,17 @@
+package com.afrobad.VeinLinker.registrationandlogin.userauthentication.service;
+
+import com.afrobad.VeinLinker.registrationandlogin.users.entity.Users;
+
+@Service
+public class JWTService {
+	
+	
+
+	    public String generateToken(Users user){
+
+	        // Generate JWT
+
+	   
+
+	}
+}
