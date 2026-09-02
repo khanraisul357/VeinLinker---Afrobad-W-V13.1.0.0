e90d952a309ba2767c515ceb139230070d8cb139
Md Raisul Islam Khan <khanraisul357@gmail.com>
2026-09-02 21:06:35 +0600

    added a query method findByUserAndDocumentType()

--- src/main/java/com/afrobad/VeinLinker/registrationandlogin/uploadedfile/repository/UserFileRepository.java
+++ src/main/java/com/afrobad/VeinLinker/registrationandlogin/uploadedfile/repository/UserFileRepository.java
@@ -4,6 +4,7 @@ package com.afrobad.VeinLinker.registrationandlogin.uploadedfile.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.afrobad.VeinLinker.registrationandlogin.uploadedfile.entity.UserFile;
+import com.afrobad.VeinLinker.registrationandlogin.uploadedfile.enums.FileType;
 import com.afrobad.VeinLinker.registrationandlogin.users.entity.Users;
 import java.util.List;
 import java.util.Optional;
@@ -11,5 +12,9 @@ import java.util.Optional;
 public interface UserFileRepository extends JpaRepository<UserFile,Long>{
 	
 	Optional<List<UserFile>> findByUser(Users user);
+	
+	//Fetching UserFile object by user & document type
+	Optional<UserFile> findByUserAndDocumentType(Users user,FileType documentType);
+
 
 }
