afacdb485a1fa6015e1efbd62e64418696beb9c7
Md Raisul Islam Khan <khanraisul357@gmail.com>
2026-09-06 00:00:06 +0600

    returns jwt token specific for a particular authenticated admin

---
+++ src/main/java/com/afrobad/VeinLinker/adminlogin/adminauthentication/dto/AdminLoginResponseDTO.java
@@ -0,0 +1,10 @@
+package com.afrobad.VeinLinker.adminlogin.adminauthentication.dto;
+
+import lombok.*;
+
+@Getter
+@AllArgsConstructor
+public class AdminLoginResponseDTO {
+
+	 private final String token;
+}
